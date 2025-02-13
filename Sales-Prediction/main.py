3d0dbbf9ed43de8ac3d477c7093c7e76c2049a83
W-Officer <WynnOfficer@westernwyoming.edu>
2025-02-13 00:23:27 +0000

    Ready for Grading.

--- Sales-Prediction/main.py
+++ Sales-Prediction/main.py
@@ -1,6 +1,6 @@
 #
-# Name
-# Date
+# Wynn Officer
+# 2/12/2025
 # Sales Prediction Programming Project
 # COSC 1010
 #
@@ -8,7 +8,8 @@
 # Variables to hold the sales total and the profit
 
 # Get the amount of projected sales.
-
+total_sales =float(input('Enter the projected sales: '))
 # Calculate the projected profit.
-
+profit = total_sales * 0.23
 # Print the projected profit.
+print('The profit is $', format(profit, ',.2f'))
