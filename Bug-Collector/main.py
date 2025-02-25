7a121256751757152f6e5a2e95f5ce3d47d3243b
W-Officer <WynnOfficer@westernwyoming.edu>
2025-02-25 21:31:42 +0000

    Ready to be Graded

--- Bug-Collector/main.py
+++ Bug-Collector/main.py
@@ -1,11 +1,16 @@
 #
-# Name
-# Date
+# Wynn Officer
+# 2/25/25
 # Bug Collector Programming Project
 # COSC 1010
 #
 # Initialize variables for bugs and total number of bugs collected.
-
+bugsCollected = 0.0
+bugs = 0.0
 # Get number of bugs collected each day using a for loop.
-
+for day in range(1,6) :
+    print ('Bugs collected on day', day)
+    bugs = int(input())
+    bugsCollected += bugs
 # Display the total number of bugs collected.
+print ('You have collected a total of', bugsCollected, 'bugs.')
