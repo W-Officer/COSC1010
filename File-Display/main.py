7781915497fc492e71a2e4d74eb872c7030454ae
W-Officer <WynnOfficer@westernwyoming.edu>
2025-03-31 23:27:05 +0000

    Ready for grading

--- File-Display/main.py
+++ File-Display/main.py
@@ -1,7 +1,18 @@
 #
-# Name
-# Date
+# Wynn Officer
+# 3/31/25
 # File Display Programming Project
 # COSC 1010
 #
 # Use comments liberally throughout the program. 
+
+#Open the file if the file exists
+myfile = open('/workspaces/COSC1010/File-Display/numbers.txt', 'r')
+
+#display the files contents
+for line in myfile:
+    number = int(line)
+    print(number)
+
+#close file
+myfile.close()
