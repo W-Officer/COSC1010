cd2fafb86896ab995f34d7ea5ac4f9d71a3ef40e
W-Officer <WynnOfficer@westernwyoming.edu>
2025-04-01 01:18:56 +0000

    Ready to grade

--- Average-of-Numbers/main.py
+++ Average-of-Numbers/main.py
@@ -1,7 +1,29 @@
 #
-# Name
-# Date
+# Wynn Officer
+# 3/31/25
 # Average of Numbers Programming Project
 # COSC 1010
 #
 # Use comments liberally throughout the program. 
+#variables
+amount = 0.0
+lines = 0.0
+average = 0.0
+
+#open the file
+myfile = open('/workspaces/COSC1010/Average-of-Numbers/numbers.txt', 'r')
+
+#get amount of number and how many loops
+for line in myfile:
+    number = int(line)
+    amount += number
+    lines += 1
+    
+#close file
+myfile.close()
+
+#get average
+average = amount/lines
+
+#print average
+print(average)
