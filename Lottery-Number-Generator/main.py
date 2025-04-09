069e6ba3590fe68c21d5d1500c2d5e06eb4c1eae
W-Officer <WynnOfficer@westernwyoming.edu>
2025-04-09 18:01:18 +0000

    Ready for grading

--- Lottery-Number-Generator/main.py
+++ Lottery-Number-Generator/main.py
@@ -1,7 +1,32 @@
 #
-# Name
-# Date
+# Wynn Officer
+# 4/9/25
 # Lottery Number Generator Programming Project
 # COSC 1010
 #
 # Use comments liberally throughout the program. 
+
+import random
+
+MAX_DIGITS = 7
+START = 0
+END = 9
+
+#function
+def main():
+    #create the list of number
+    numbers = [0] * 7
+
+    #populate with random numbers
+    for index in range(MAX_DIGITS) :
+        numbers[index] = random.randint (START, END)
+
+    #print the numbers
+    print('The Lottery numbers are:')
+    for index in range(MAX_DIGITS) :
+        print(numbers[index], end='')
+    print()
+
+#call the function
+main()
+
--- Magic-8-Ball/main.py
+++ Magic-8-Ball/main.py
@@ -1,7 +1,40 @@
 #
-# Name
-# Date
+# Wynn Officer
+# 4/9/25
 # Magic 8 Ball Programming Project
 # COSC 1010
 #
 # Use comments liberally throughout the program. 
+
+import random
+
+#function for loading responses
+def load_responses():
+    #load responses from file
+    try:
+        with open('Magic-8-Ball/8_ball_responses.txt', 'r') as file:
+            responses = file.readlines()
+
+        return responses
+    #check for any IOError
+    except IOError:
+        print('Error: Could not read file.')
+    
+#function for running the game
+def main():
+    responses = load_responses()
+
+    while True:
+        question = input("Ask a yes or no question (or type 'quit' to exit): ")
+
+        #check if user wants to quit
+        if question == 'quit':
+            print('Thanks for playing!')
+            break
+
+        #Randomly select responses
+        response = random.choice(responses)
+        print('The Magic 8 Ball says: ', response)
+
+#start the game
+main()
