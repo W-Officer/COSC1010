709ddcd3d71da44b47687e3efcb1b8ea858cad1f
W-Officer <WynnOfficer@westernwyoming.edu>
2025-04-20 01:31:05 +0000

    Ready for grading

--- Pig-Latin/main.py
+++ Pig-Latin/main.py
@@ -1,7 +1,29 @@
 #
-# Name
-# Date
+# Wynn Officer
+# 4/19/25
 # Pig Latin Programming Project
 # COSC 1010
 #
 # Use comments liberally throughout the program. 
+
+#call user input
+usr_string = input('Enter a sentence: ')
+
+#split the sentence into words
+words = usr_string.split()
+
+#create a list
+words_pig = []
+
+#Convert word into pig latin
+for word in words:
+    #take first letter and add it to the end, also add "ay"
+    pig_word = word[1:] + word[0] + "Ay"
+
+    #put word into list
+    words_pig.append(pig_word)
+
+pig_latin_sentence = ' '.join(words_pig)
+
+#print the function
+print('Pig Latin: ', pig_latin_sentence)
--- Vowels-and-Consonants/main.py
+++ Vowels-and-Consonants/main.py
@@ -1,7 +1,51 @@
 #
-# Name
-# Date
+# Wynn Officer
+# 4/19/25
 # Vowels and Consonants Programming Project
 # COSC 1010
 #
 # Use comments liberally throughout the program. 
+
+#main function.
+def main():
+    #get the string from user.
+    user_str = input('Enter a string of characters: ')
+
+    #print the vowels and constantants
+    print('That string has', num_vowels(user_str), 'vowels and', \
+          num_constantants(user_str), 'constantants.')
+
+#vowel function.
+def num_vowels(s):
+    #make a list of vowels
+    vowels = ['a', 'e', 'i', 'o', 'u']
+
+    #starting accumulator
+    v_count = 0
+
+    #count the vowels in s.
+    for ch in s:
+        if ch.lower() in vowels:
+            v_count += 1
+
+    #return vowel count.
+    return v_count
+
+#constantant function.
+def num_constantants(s):
+    #make another vowel list
+    vowels = ['a', 'e', 'i', 'o', 'u']
+
+    #start accumulator
+    c_count = 0
+
+    #count the constantants in s.
+    for ch in s:
+        if ch.isalpha() and ch.lower() not in vowels:
+            c_count += 1
+
+    #return the consonant count.
+    return c_count
+
+#call main function
+main()
