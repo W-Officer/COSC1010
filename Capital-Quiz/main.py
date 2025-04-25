6921701634a2af3123d114ffcb833a45e9ca9396
W-Officer <WynnOfficer@westernwyoming.edu>
2025-04-25 04:23:02 +0000

    Ready to grade!

--- Capital-Quiz/main.py
+++ Capital-Quiz/main.py
@@ -1,6 +1,6 @@
 #
-# Name
-# Date
+# Wynn Officer
+# 4/24/25
 # Capital Quiz Programming Project
 # COSC 1010
 #
@@ -8,37 +8,68 @@
 
 import random
 
+# Initialize dictionary
+capitals = {'Alabama':'Montgomery', 'Alaska':'Juneau',
+            'Arizona':'Phoenix', 'Arkansas':'Little Rock',
+            'California':'Sacramento', 'Colorado':'Denver',
+            'Connecticut':'Hartford', 'Delaware':'Dover',
+            'Florida':'Tallahassee', 'Georgia':'Atlanta',
+            'Hawaii':'Honolulu', 'Idaho':'Boise',
+            'Illinois':'Springfield', 'Indiana':'Indianapolis',
+            'Iowa':'Des Moines', 'Kansas':'Topeka',
+            'Kentucky':'Frankfort', 'Louisiana':'Baton Rouge',
+            'Maine':'Augusta', 'Maryland':'Annapolis',
+            'Massachusetts':'Boston', 'Michigan':'Lansing',
+            'Minnesota':'Saint Paul', 'Mississippi':'Jackson',
+            'Missouri':'Jefferson City', 'Montana':'Helena',
+            'Nebraska':'Lincoln', 'Nevada':'Carson City',
+            'New Hampshire':'Concord', 'New Jersey':'Trenton',
+            'New Mexico':'Santa Fe', 'New York':'Albany',
+            'North Carolina':'Raleigh', 'North Dakota':'Bismarck',
+            'Ohio':'Columbus', 'Oklahoma':'Oklahoma City',
+            'Oregon':'Salem', 'Pennsylvania':'Harrisburg',
+            'Rhode Island':'Providence', 'South Carolina':'Columbia',
+            'South Dakota':'Pierre', 'Tennessee':'Nashville',
+            'Texas':'Austin', 'Utah':'Salt Lake City',
+            'Vermont':'Montpelier', 'Virginia':'Richmond',
+            'Washington':'Olympia', 'West Virginia':'Charleston',
+            'Wisconsin':'Madison', 'Wyoming':'Cheyenne'}
+    
 def main():
-    # Initialize dictionary
-    capitals = {'Alabama':'Montgomery', 'Alaska':'Juneau',
-                'Arizona':'Phoenix', 'Arkansas':'Little Rock',
-                'California':'Sacramento', 'Colorado':'Denver',
-                'Connecticut':'Hartford', 'Delaware':'Dover',
-                'Florida':'Tallahassee', 'Georgia':'Atlanta',
-                'Hawaii':'Honolulu', 'Idaho':'Boise',
-                'Illinois':'Springfield', 'Indiana':'Indianapolis',
-                'Iowa':'Des Moines', 'Kansas':'Topeka',
-                'Kentucky':'Frankfort', 'Louisiana':'Baton Rouge',
-                'Maine':'Augusta', 'Maryland':'Annapolis',
-                'Massachusetts':'Boston', 'Michigan':'Lansing',
-                'Minnesota':'Saint Paul', 'Mississippi':'Jackson',
-                'Missouri':'Jefferson City', 'Montana':'Helena',
-                'Nebraska':'Lincoln', 'Nevada':'Carson City',
-                'New Hampshire':'Concord', 'New Jersey':'Trenton',
-                'New Mexico':'Santa Fe', 'New York':'Albany',
-                'North Carolina':'Raleigh', 'North Dakota':'Bismarck',
-                'Ohio':'Columbus', 'Oklahoma':'Oklahoma City',
-                'Oregon':'Salem', 'Pennsylvania':'Harrisburg',
-                'Rhode Island':'Providence', 'South Carolina':'Columbia',
-                'South Dakota':'Pierre', 'Tennessee':'Nashville',
-                'Texas':'Austin', 'Utah':'Salt Lake City',
-                'Vermont':'Montpelier', 'Virginia':'Richmond',
-                'Washington':'Olympia', 'West Virginia':'Charleston',
-                'Wisconsin':'Madison', 'Wyoming':'Cheyenne'}
-
     # Local variables
+    state_caps = list(capitals.keys())
+    correct = 0
+    incorrect = 0
 
-    # Continue until user quits the game.
+    #quiz the user
+    while True:
+        #get an entry at random from the dictionary
+        state = random.choice(state_caps)
+
+         #ask the question?
+        print('What is the capital of ', state, '? (Type "quit" to end the quiz): ', end='')
+        response = input().strip()
+
+        #check if user wants to quit
+        if response.lower()  == 'quit':
+            break
+
+        #indicitate correct response
+        correct_answer = capitals[state]
+        if response.lower() == correct_answer.lower():
+            correct += 1
+            print('Correct!')
+        
+        #indicate an incorrect response
+        else:
+            incorrect += 1
+            print('Incorrect.')
+        
+    
+#print the results
+    print('Correct responses:', correct)
+    print('Incorrect responses:', incorrect)
+
 
 # Call the main function.
 main()
